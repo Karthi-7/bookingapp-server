bfa6c2630e7a174192f83067e74e129ab6d6416e
Karthi-7 <karthikeyancvl96@gmail.com>
2023-01-10 00:01:50 +0530

    get req

--- routes/property.routes.js
+++ routes/property.routes.js
@@ -2,7 +2,7 @@ const controller=require("../controller/property.controller")
 
 const Routes=(app)=>{
     app.get("/orbizRooms/getdata",controller.getData)
-    app.post("/orbizRooms/getroomById",controller.getDataById)
+    app.get("/orbizRooms/getroomById",controller.getDataById)
     app.post("/orbizrooms/addroomdata",controller.addRooms)
  
 
