5a1bd49b137bea6040da08258183ce33c908d268
Karthi-7 <karthikeyancvl96@gmail.com>
2023-01-09 21:09:49 +0530

    cross origin fix

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const cors=require("cors")
 var bodyParser = require('body-parser')
 const PORT= 5000
 
-app.use(cors())
+app.use(cors({
+    origin:"https://orbiz-rooms-client.onrender.com"
+}))
 //import routes
 const PropertyRoutes=require("./routes/property.routes")
 const AuthRoutes=require("./routes/auth.routes")
